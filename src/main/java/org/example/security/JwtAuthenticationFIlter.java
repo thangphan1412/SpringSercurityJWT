f29fc52dd831acfb61d1c752323789ab5e292925
thangphan14122003@gmail.com <thangphan14122003@gmail.com>
2025-04-18 15:11:49 +0700

    more

--- src/main/java/org/example/security/JwtAuthenticationFIlter.java
+++ src/main/java/org/example/security/JwtAuthenticationFIlter.java
@@ -6,17 +6,36 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.constraints.NotNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
+// để Spring Boot tự động phát hiện và quản lý Bean này
+@Component
+//tự tạo constructor cho các field final (nếu có).
+@RequiredArgsConstructor
+//OncePerRequestFilter: class cha đảm bảo filter này chỉ được gọi một lần duy nhất mỗi request.
 public class JwtAuthenticationFIlter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+
     @Override
+  //phương thức sẽ được gọi tự động trong chuỗi xử lý request của Spring Security.
     protected void doFilterInternal(
                                     @NonNull HttpServletRequest request,
                                    @NonNull HttpServletResponse response,
                                    @NonNull FilterChain filterChain
     ) throws ServletException, IOException {
-
+        final String authHeader = request.getHeader("Authorization");
+        final String token;
+        final String userEmail;
+        if(authHeader == null || !authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+        token = authHeader.substring(7);
+        userEmail = jwtService.extractUsername(token);
     }
 }
---
+++ src/main/java/org/example/security/JwtService.java
@@ -0,0 +1,10 @@
+package org.example.security;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtService {
+    public String extractUsername(String token) {
+        return null;
+    }
+}
